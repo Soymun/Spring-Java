4e8cd7c798465885fc2ae60ff66a7eebc8f3f30b
Soymun <pkekeoke@mail.ru>
2022-09-25 17:12:54 +0300

    Homework №4

--- src/main/java/com/edu/ulab/app/mapper/UserMapper.java
+++ src/main/java/com/edu/ulab/app/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.edu.ulab.app.mapper;
 
 import com.edu.ulab.app.dto.UserDto;
+import com.edu.ulab.app.entity.Person;
+import com.edu.ulab.app.web.request.Update.UserRequestUpdate;
 import com.edu.ulab.app.web.request.UserRequest;
 import org.mapstruct.Mapper;
 
@@ -9,4 +11,10 @@ public interface UserMapper {
     UserDto userRequestToUserDto(UserRequest userRequest);
 
     UserRequest userDtoToUserRequest(UserDto userDto);
+
+    Person userDtoToPerson(UserDto userDto);
+
+    UserDto personToUserDto(Person person);
+
+    UserDto userRequestUpdateToUserDto(UserRequestUpdate userRequestUpdate);
 }
